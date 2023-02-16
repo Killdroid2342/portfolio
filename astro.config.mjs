2789522366f6cd12f3ed2d8313780ec39e89b7c2
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-16 00:02:19 +0000

    installed tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [react()],
+  integrations: [react(), tailwind()]
 });
