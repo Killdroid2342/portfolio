28823ab67b06856ef7df2e476f4ef08252ed6fda
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-28 00:57:45 +0000

    fixed nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,12 +5,16 @@ const Nav = () => {
     <div>
       <nav className='mt-10 mr-40'>
         <ul className='flex flex-row justify-end'>
-          <li className='p-2 cursor-pointer text-purple-900 font-bold mr-10'>
-            <a href='https://github.com/Killdroid2342' target='_blank'>
+          <li className='p-2 text-white font-bold mr-10'>
+            <a
+              href='https://github.com/Killdroid2342'
+              target='_blank'
+              className='cursor-none'
+            >
               Github
             </a>
           </li>
-          <li className='hover:bg-purple-900 hover:text-white border-2 rounded-2xl border-purple-900 p-2 font-bold text-purple-900 cursor-pointer transition-all duration-500'>
+          <li className='hover:bg-purple-900 border-2 rounded-2xl border-white p-2 font-bold text-white transition-all duration-500'>
             Contact
           </li>
         </ul>
