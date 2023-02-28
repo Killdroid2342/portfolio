f619ca539299b88b86c2cfd02cb4a8feb0decbc7
Haroon Khan <HaroonKhan120704@gmail.com>
2023-02-28 00:57:38 +0000

    fixed cursor bug and fixed images

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const FrontPage = () => {
   return (
     <div>
-      <h1 className='font-bold text-6xl text-center mt-56 text-purple-900'>
+      <h1 className='font-bold text-6xl text-center mt-56 text-white'>
         Frontend Developer
       </h1>
-      <p className='text-center mt-20 text-purple-900 font-bold text-lg'>
+      <p className='text-center mt-20 text-white font-bold text-lg'>
         I love to code and implement an idea into reality.
       </p>
       <div className='flex items-center justify-center mt-10'>
-        <button className='hover:bg-purple-900 hover:text-white border-2 rounded-2xl border-purple-900 p-2 font-bold text-purple-900 cursor-pointer transition-all duration-500 mt-10'>
+        <button className='hover:bg-purple-900 border-2 rounded-2xl border-white p-2 font-bold text-white transition-all duration-500 cursor-none mt-10'>
           See More
         </button>
       </div>
--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 const MyWork = () => {
   return (
     <>
-      <h2 className='text-center text-4xl font-bold text-purple-900'>
+      <h2 className='text-center text-4xl font-bold text-white'>
         Some Of My Work
       </h2>
-      <p className='text-center text-purple-900 mt-6 mb-10'>
+      <p className='text-center text-white mt-6 mb-10'>
         This is some of the work that I have created
       </p>
       <div className='flex flex-col justify-center items-center'>
         <div className='flex flex-row'>
-          <div className='border-2 border-purple-900 rounded-lg p-32 m-5 cursor-pointer relative shadow-xl'>
+          <div className='border-2 border-white rounded-lg p-32 m-5 cursor-none relative shadow-xl'>
             <img
               src='/src/assets/img/Calendar.png'
               alt='Calendar'
@@ -21,12 +21,13 @@ const MyWork = () => {
               <a
                 href='https://calendar-three-liard.vercel.app/'
                 target='_blank'
+                className='cursor-none'
               >
                 Calendar
               </a>
             </div>
           </div>
-          <div className='border-2 border-purple-900 rounded-lg p-32 m-5 cursor-pointer relative shadow-xl'>
+          <div className='border-2 border-white rounded-lg p-32 m-5 cursor-none relative shadow-xl'>
             <img
               src='/src/assets/img/Covid.png'
               alt='Covid Tracker'
@@ -36,45 +37,58 @@ const MyWork = () => {
               <a
                 href='https://covid-tracker-six-kappa.vercel.app/'
                 target='_blank'
+                className='cursor-none'
               >
                 Covid Tracker
               </a>
             </div>
           </div>
-          <div className='border-2 border-purple-900 rounded-lg p-32 m-5 cursor-pointer relative shadow-xl'>
+          <div className='border-2 border-white rounded-lg p-32 m-5 cursor-none relative shadow-xl'>
             <img
               src='/src/assets/img/Joke.png'
               alt='Joke App'
               className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
             ></img>
             <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
-              <a href='https://joke-app-woad.vercel.app/' target='_blank'>
+              <a
+                href='https://joke-app-woad.vercel.app/'
+                target='_blank'
+                className='cursor-none'
+              >
                 Joke App
               </a>
             </div>
           </div>
         </div>
         <div className='flex flex-row'>
-          <div className='border-2 border-purple-900 rounded-lg p-32 m-5 cursor-pointer relative shadow-xl'>
+          <div className='border-2 border-white rounded-lg p-32 m-5 cursor-none relative shadow-xl'>
             <img
               src='/src/assets/img/To-Do-List.png'
               alt='To Do List'
               className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
             ></img>
             <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
-              <a href='https://to-do-app-lac.vercel.app/' target='_blank'>
+              <a
+                href='https://to-do-app-lac.vercel.app/'
+                target='_blank'
+                className='cursor-none'
+              >
                 To Do List
               </a>
             </div>
           </div>
-          <div className='border-2 border-purple-900 rounded-lg p-32 m-5 cursor-pointer relative shadow-xl'>
+          <div className='border-2 border-white rounded-lg p-32 m-5 cursor-none relative shadow-xl'>
             <img
-              src='/src/assets/img/Random Colour.png'
+              src='/src/assets/img/RandomColor.png'
               alt='Random Colour'
               className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
             ></img>
             <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
-              <a href='https://random-colour.vercel.app/' target='_blank'>
+              <a
+                href='https://random-colour.vercel.app/'
+                target='_blank'
+                className='cursor-none'
+              >
                 Random Colour
               </a>
             </div>
