c87e0ee6dde823332c96bf26d868406c549aa483
Haroon Khan <HaroonKhan120704@gmail.com>
2023-03-16 22:09:53 +0000

    added new section

--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -11,17 +11,19 @@ const MyWork = () => {
       </p>
       <div className='flex flex-col justify-center items-center pt-10 border border-red-900'>
         <div className='flex flex-col 2xl:flex-row'>
-          <p className='text-center text-white font-bold text-xl'>Calendar</p>
           <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
             <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
               <img
-                src='/src/assets/img/Calendar.png'
-                alt='Calendar'
+                src='/src/assets/img/Joke.png'
+                alt='Joke App'
                 className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
               ></img>
               <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
                 <a
-                  href='https://calendar-three-liard.vercel.app/'
+                  href='https://joke-app-woad.vercel.app/'
                   target='_blank'
                   className='cursor-none'
                 >
@@ -30,10 +32,10 @@ const MyWork = () => {
               </div>
             </div>
           </div>
-          <p className='text-center text-white font-bold text-xl'>
-            Covid Tracker
-          </p>
           <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
             <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
               <img
                 src='/src/assets/img/Covid.png'
@@ -51,8 +53,10 @@ const MyWork = () => {
               </div>
             </div>
           </div>
-          <p className='text-center text-white font-bold text-xl'>Joke App</p>
           <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
             <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
               <img
                 src='/src/assets/img/Joke.png'
@@ -72,10 +76,31 @@ const MyWork = () => {
           </div>
         </div>
         <div className='flex flex-col 2xl:flex-row'>
-          <p className='text-center text-white font-bold text-xl'>
-            Covid Tracker
-          </p>
           <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
+            <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
+              <img
+                src='/src/assets/img/Joke.png'
+                alt='Joke App'
+                className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
+              ></img>
+              <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
+                <a
+                  href='https://joke-app-woad.vercel.app/'
+                  target='_blank'
+                  className='cursor-none'
+                >
+                  Click Here To See Project
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
             <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
               <img
                 src='/src/assets/img/Covid.png'
@@ -93,8 +118,10 @@ const MyWork = () => {
               </div>
             </div>
           </div>
-          <p className='text-center text-white font-bold text-xl'>Joke App</p>
           <div className='w-full md:w-1/2 lg:w-1/3'>
+            <p className='text-center text-white font-bold text-xl'>
+              Covid Tracker
+            </p>
             <div className='border-2 border-white rounded-lg p-20 md:p-40 m-20 cursor-none relative shadow-xl'>
               <img
                 src='/src/assets/img/Joke.png'
