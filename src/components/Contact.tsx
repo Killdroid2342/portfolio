1aede562ce89f2c4fd415954bef146ac51b77c37
Haroon Khan <HaroonKhan120704@gmail.com>
2023-03-18 22:58:24 +0000

    removing particles

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div id="contact" className="flex justify-center">
-      <div className="bg-white border rounded-lg w-96 max-w-full shadow-xl">
-        <h2 className="text-2xl font-bold text-purple-900 text-center p-10 ">
+    <div id='contact' className='flex justify-center'>
+      <div className='bg-white border rounded-lg w-96 max-w-full shadow-xl'>
+        <h2 className='text-2xl font-bold text-purple-900 text-center p-10 '>
           Contact
         </h2>
-        <div className="flex justify-center">
+        <div className='flex justify-center'>
           <img
-            src="src/assets/img/logo-gmail-9987.png"
-            alt="Visual Studio Code"
+            src='src/assets/img/logo-gmail-9987.png'
+            alt='Visual Studio Code'
             width={50}
           />
         </div>
-        <p className="text-center font-bold p-10">
+        <p className='text-center font-bold p-10'>
           One of the best ways to contact me is through email. I regularly check
           my email and will be sure to respond if you send me a message.
         </p>
-        <p className=" text-purple-900 font-bold text-center">Email:</p>
-        <p className="text-center font-bold p-10">HaroonKhan120704@gmail.com</p>
+        <p className=' text-purple-900 font-bold text-center'>Email:</p>
+        <p className='text-center font-bold p-10'>HaroonKhan120704@gmail.com</p>
       </div>
     </div>
   );
--- src/components/MyWork.tsx
+++ src/components/MyWork.tsx
@@ -1,73 +1,73 @@
-import React from "react";
+import React from 'react';
 const PROJECTS = [
   {
-    name: "Calendar",
+    name: 'Calendar',
     image: {
-      src: "/src/assets/img/Calendar.png",
-      alt: "Calendar",
+      src: '/src/assets/img/Calendar.png',
+      alt: 'Calendar',
     },
-    href: "https://calendar-three-liard.vercel.app/",
+    href: 'https://calendar-three-liard.vercel.app/',
   },
   {
-    name: "Covid Tracker",
+    name: 'Covid Tracker',
     image: {
-      src: "/src/assets/img/Covid.png",
-      alt: "Covid Tracker",
+      src: '/src/assets/img/Covid.png',
+      alt: 'Covid Tracker',
     },
-    href: "https://covid-tracker-six-kappa.vercel.app/",
+    href: 'https://covid-tracker-six-kappa.vercel.app/',
   },
   {
-    name: "Joke App",
+    name: 'Joke App',
     image: {
-      src: "/src/assets/img/Joke.png",
-      alt: "Joke App",
+      src: '/src/assets/img/Joke.png',
+      alt: 'Joke App',
     },
-    href: "https://joke-app-woad.vercel.app/",
+    href: 'https://joke-app-woad.vercel.app/',
   },
   {
-    name: "To Do List",
+    name: 'To Do List',
     image: {
-      src: "/src/assets/img/To-Do-List.png",
-      alt: "To Do List",
+      src: '/src/assets/img/To-Do-List.png',
+      alt: 'To Do List',
     },
-    href: "https://to-do-app-lac.vercel.app/",
+    href: 'https://to-do-app-lac.vercel.app/',
   },
   {
-    name: "Random Colour",
+    name: 'Random Colour',
     image: {
-      src: "/src/assets/img/RandomColor.png",
-      alt: "Random Colour",
+      src: '/src/assets/img/RandomColor.png',
+      alt: 'Random Colour',
     },
-    href: "https://random-colour-r4ctjrmzt-killdroid2342.vercel.app/",
+    href: 'https://random-colour-r4ctjrmzt-killdroid2342.vercel.app/',
   },
 ];
 const MyWork = () => {
   return (
     <>
-      <h2 className="text-center text-4xl font-bold text-white">
+      <h2 className='text-center text-4xl font-bold text-white'>
         Some Of My Work
       </h2>
-      <p className="text-center text-white mt-6 mb-20">
+      <p className='text-center text-white mt-6 mb-20'>
         This is some of the work that I have created
       </p>
-      <ul className="flex flex-wrap justify-center items-center pt-10 gap-32 w-[1380px] mx-auto max-w-full">
-        {PROJECTS.map((project) => {
+      <ul className='flex flex-wrap justify-center items-center pt-10 gap-32 w-[1380px] mx-auto max-w-full'>
+        {PROJECTS.map((project, i) => {
           return (
-            <li>
-              <p className="text-center text-white font-bold text-xl">
+            <li key={i}>
+              <p className='text-center text-white font-bold text-xl'>
                 {project.name}
               </p>
-              <div className="border-2 border-white rounded-lg cursor-none relative shadow-xl h-72 w-72">
+              <div className='border-2 border-white rounded-lg cursor-none relative shadow-xl h-72 w-72'>
                 <img
                   src={project.image.src}
                   alt={project.image.alt}
-                  className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
+                  className='absolute top-0 left-0 w-full h-full object-cover rounded-lg'
                 ></img>
-                <div className="text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg">
+                <div className='text-xl font-bold text-white absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 hover:opacity-90 duration-200 hover:bg-purple-900 rounded-lg'>
                   <a
                     href={project.href}
-                    target="_blank"
-                    className="cursor-none"
+                    target='_blank'
+                    className='cursor-none'
                   >
                     Click Here To See Project
                   </a>
