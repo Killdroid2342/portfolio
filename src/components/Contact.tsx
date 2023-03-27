67c1e6e1ef241d2fb189f240fcf22d138b3d1f43
Haroon Khan <HaroonKhan120704@gmail.com>
2023-03-27 02:52:32 +0100

    re doing contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,24 +2,23 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div id='contact' className='flex justify-center'>
-      <div className='bg-white border rounded-lg w-96 max-w-full shadow-xl'>
-        <h2 className='text-2xl font-bold text-purple-900 text-center p-10 '>
-          Contact
-        </h2>
-        <div className='flex justify-center'>
-          <img
-            src='src/assets/img/logo-gmail-9987.png'
-            alt='Visual Studio Code'
-            width={50}
-          />
+    <div id='contact'>
+      <div className='section'>
+        <h2 className='headers'>Contact</h2>
+        <div className='flex flex-row justify-center'>
+          <div>
+            <div className='border border-white rounded-2xl'>Pic</div>
+            <p>Email</p>
+          </div>
+          <div>
+            <div className='border border-white rounded-2xl'>Pic</div>
+            <p>Linked In</p>
+          </div>
+          <div>
+            <div className='border border-white rounded-2xl'>Pic</div>
+            <p>Github</p>
+          </div>
         </div>
-        <p className='text-center font-bold p-10'>
-          One of the best ways to contact me is through email. I regularly check
-          my email and will be sure to respond if you send me a message.
-        </p>
-        <p className=' text-purple-900 font-bold text-center'>Email:</p>
-        <p className='text-center font-bold p-10'>HaroonKhan120704@gmail.com</p>
       </div>
     </div>
   );
