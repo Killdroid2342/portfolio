e2180c1f8814894e14d21f7b097fc65f0681e633
Haroon Khan <HaroonKhan120704@gmail.com>
2023-04-04 00:25:58 +0100

    added some jsx

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Loader.css';
+export default function Loader() {
+  return (
+    <>
+      <div className='loader'>
+        <div className='scanner'>
+          <span>Loading...</span>
+        </div>
+      </div>
+    </>
+  );
+}
