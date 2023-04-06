72b6d21a2c41cc58f9db812baa39eae0008be9ba
Haroon Khan <HaroonKhan120704@gmail.com>
2023-04-06 02:55:13 +0100

    added new tailwind media queries

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    screens: {
+      '2xl': { max: '1535px' },
+
+      xl: { max: '1279px' },
+
+      lg: { max: '1023px' },
+
+      md: { max: '767px' },
+
+      sm: { max: '639px' },
+    },
   },
   plugins: [],
 };
